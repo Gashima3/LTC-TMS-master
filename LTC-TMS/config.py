1b7f0c9ee5007d1dc6e69e2f311f4a89d4a3b90f
Gashima3 <37646616+Gashima3@users.noreply.github.com>
2018-07-26 16:56:53 +0800

    config.py ready

---
+++ LTC-TMS/config.py
@@ -0,0 +1,10 @@
+
+
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'icac104104'
+MYSQL_DB = 'test1'
+MYSQL_CURSORCLASS = 'DictCursor'
+
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:icac104104@localhost:3306/test1'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
