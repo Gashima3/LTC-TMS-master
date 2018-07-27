1ce465a06cdac51bb420b6d72fda3dfea21841b9
Gashima3 <37646616+Gashima3@users.noreply.github.com>
2018-07-27 16:42:03 +0800

    Testing success

--- LTC-TMS/Test.py
+++ LTC-TMS/Test.py
@@ -1,14 +1,14 @@
-from flask import Flas
+from flask import Flask
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = 'ec2-18-191-67-10.us-east-2.compute.amazonaws.com'
 app.config['MySQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'icac104104'
 app.config['MYSQL_DB'] = 'test1'
 app.config['MYSQL_CURSORCLASS'] = 'DictCrusor'
 
-mysql = MYSQL(app)
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
