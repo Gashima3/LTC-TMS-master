4ddfc3ce355b65ca516d5c6f6d90779a3261a1a2
Gashima3 <37646616+Gashima3@users.noreply.github.com>
2018-07-27 17:20:45 +0800

    Testchanges

--- LTC-TMS/Test.py
+++ LTC-TMS/Test.py
@@ -2,21 +2,21 @@ from flask import Flask
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'ec2-18-191-67-10.us-east-2.compute.amazonaws.com'
+app.config['MYSQL_HOST'] = 'localhost'
 app.config['MySQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'icac104104'
 app.config['MYSQL_DB'] = 'test1'
-app.config['MYSQL_CURSORCLASS'] = 'DictCrusor'
+#app.config['MYSQL_CURSORCLASS'] = 'DictCrusor'
 
 mysql = MySQL(app)
 
 @app.route('/')
 def index():
     cur = mysql.connect.cursor()
-    cur.execute('SELECT * FROM exmaple ')
+    cur.execute('SELECT * FROM example ')
     rv = cur.fetchall()
     return '<h1>' + str(rv) + '</h1>'
 
 
 if __name__ =='__main__':
-    app.run(debug=True)
+    app.run(port=80, host="0.0.0.0")
