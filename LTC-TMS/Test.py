eddd7dafce4ec8096bf850ab213fe5e72f3eb4c6
Gashima3 <37646616+Gashima3@users.noreply.github.com>
2018-07-27 11:45:58 +0800

    Testing DB-Flask

--- LTC-TMS/Test.py
+++ LTC-TMS/Test.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flas
+from flask_mysqldb import MySQL
+
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MySQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'icac104104'
+app.config['MYSQL_DB'] = 'test1'
+app.config['MYSQL_CURSORCLASS'] = 'DictCrusor'
+
+mysql = MYSQL(app)
 
 @app.route('/')
-def hello_flask():
-    return 'Hello WORLD'
+def index():
+    cur = mysql.connect.cursor()
+    cur.execute('SELECT * FROM exmaple ')
+    rv = cur.fetchall()
+    return '<h1>' + str(rv) + '</h1>'
+
 
 if __name__ =='__main__':
     app.run(debug=True)
