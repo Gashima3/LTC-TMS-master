7b3dd661a71f9ada8932772ff0a22a5988f27f40
felixkkong <felixkkong@hotmail.com>
2018-07-27 15:13:42 +0800

    justtrying

--- LTC-TMS/Function/Login.py
+++ LTC-TMS/Function/Login.py
@@ -8,7 +8,7 @@
 
 import bcrypt
 import database
-from DBsetting.LTCTMSforms import Patient, Staff
+from DBsetting.LTCTMSmodels import Patient, Staff
 from flask_login import login_user
 import app
 
