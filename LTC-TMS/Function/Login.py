015fc87197284e3deb0755a92a11cec107455688
Gashima3 <37646616+Gashima3@users.noreply.github.com>
2018-08-02 17:25:38 +0800

    kk

--- LTC-TMS/Function/Login.py
+++ LTC-TMS/Function/Login.py
@@ -24,7 +24,7 @@ def load_user(id):
             return Staff.query.get(int(id))
     return None
 
-# requirement 38
+
 # root function for login verification
 def verifyMain(staffID, password):
     acc = requestHash(staffID)
