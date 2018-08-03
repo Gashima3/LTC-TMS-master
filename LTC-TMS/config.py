78f018fca97a9bf20b223b18e5e07944fa958009
felixkkong <felixkkong@hotmail.com>
2018-08-03 17:12:54 +0800

    justtry

--- LTC-TMS/config.py
+++ LTC-TMS/config.py
@@ -5,7 +5,7 @@ SECRET_KEY = 'wefb292h3d9be#@@YEBBCE2NION32UDFEBUE2U202hfeu2onwdsjdf'
 MYSQL_HOST = 'localhost'
 MYSQL_USER = 'root'
 MYSQL_PASSWORD = 'icac104104'
-MYSQL_DB = 'test1'
+MYSQL_DB = 'LTCTMS1'
 MYSQL_CURSORCLASS = 'DictCursor'
 
 SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:icac104104@localhost:3306/LTCTMS1'
