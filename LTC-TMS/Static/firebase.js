4edbcf6eb05eb6651c928c1fa9c0ba89f36155ce
felixkkong <felixkkong@hotmail.com>
2018-08-09 12:47:42 +0800

    firebaseedit

---
+++ LTC-TMS/Static/firebase.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/5.3.1/firebase.js"></script>
+<script>
+	// Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyDDUOzOH-upflzZvvxeUU_Nsu5c33zbwBs",
+		authDomain: "ltctms-399e4.firebaseapp.com",
+		databaseURL: "https://ltctms-399e4.firebaseio.com",
+		projectId: "ltctms-399e4",
+		storageBucket: "",
+		messagingSenderId: "229950806176"
+	};
+	firebase.initializeApp(config);
+</script>
